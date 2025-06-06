ba3d224ed0df7b79cfdd06574d5203a0e7839dd1
ftMadrid <fernandomadridcv04@gmail.com>
2025-06-06 16:55:08 -0600

    Update Memorama.java

--- Memorama/src/memorama/Memorama.java
+++ Memorama/src/memorama/Memorama.java
@@ -3,6 +3,11 @@ package memorama;
 public class Memorama {
 
     public static void main(String[] args) {
+        
+        /* 
+        Carlos Fernando Madrid Valdiviezo 22511215
+        Aaron Sebastian Cerrato Chinchilla 22451020
+        */
 
         Tablero tablero = new Tablero();
         tablero.setVisible(true);
