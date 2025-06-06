dba01bfcb949e93d799266ba2621449038d3ea9a
ftMadrid <fernandomadridcv04@gmail.com>
2025-06-06 13:20:36 -0600

    push files

--- Memorama/src/memorama/Memorama.java
+++ Memorama/src/memorama/Memorama.java
@@ -4,6 +4,10 @@ public class Memorama {
 
     public static void main(String[] args) {
         
+        Tablero tablero = new Tablero();
+        tablero.setVisible(true);
+        tablero.setLocationRelativeTo(null);
+        
     }
     
 }
